7f8b36dbad4af918a5b66044ae23d0ddce37a87c
Artem Mufazalov <artemmufazalov6197@gmail.com>
2022-08-14 17:41:37 +0300

    Fix backend url

--- src/heplers/getSelfUrl.ts
+++ src/heplers/getSelfUrl.ts
@@ -1,8 +1,23 @@
 export const getSelfUrl = (): string => {
-	return (
-		process.env.NEXT_PUBLIC_VERCEL_URL ||
-		process.env.VERCEL_URL ||
-		process.env.APP_SELF_URL ||
-		'http://localhost:3000'
-	);
+	const checkValues = ['http', 'react-caffe'];
+
+	const checkIfEnvIsUrl = (url?: string) => {
+		if (!url) {
+			return false;
+		}
+
+		return checkValues.every((v) => url.includes(v));
+	};
+
+	if (checkIfEnvIsUrl(process.env.NEXT_PUBLIC_VERCEL_URL)) {
+		return process.env.NEXT_PUBLIC_VERCEL_URL as string;
+	}
+	if (checkIfEnvIsUrl(process.env.VERCEL_URL)) {
+		return process.env.VERCEL_URL as string;
+	}
+	if (checkIfEnvIsUrl(process.env.APP_SELF_URL)) {
+		return process.env.APP_SELF_URL as string;
+	}
+
+	return 'http://localhost:3000';
 };
